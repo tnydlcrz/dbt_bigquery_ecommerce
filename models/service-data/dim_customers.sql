f99f03deb0dc10c0577d8d4eb96900dfa465c71a
Norberto Antonio Britez <tnydlcrz@gmail.com>
2023-11-17 02:01:44 +0000

    excluded geolocation table

--- models/service-data/dim_customers.sql
+++ models/service-data/dim_customers.sql
@@ -1,13 +1,14 @@
 {{ config(materialized="table") }}
 
 SELECT
-    c.customer_id,
-    c.customer_unique_id,
-    c.customer_zip_code_prefix,
-    c.customer_city,
-    c.customer_state,
-    g.geolocation_lat as customer_geo_lat,
-    g.geolocation_lng as customer_geo_lng
+    c.customer_id
+  , c.customer_unique_id
+  , c.customer_zip_code_prefix
+  , c.customer_city
+  , c.customer_state
+  --, g.geolocation_lat as customer_geo_lat
+  --, g.geolocation_lng as customer_geo_lng
+  --, row_number() over(partition by c.customer_id order by customer_id) as ord
 FROM {{ ref('customers') }} c
-    LEFT JOIN {{ ref('geolocation') }} g
-        ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
+    --LEFT JOIN {{ ref('geolocation') }} g
+        --ON c.customer_zip_code_prefix = g.geolocation_zip_code_prefix
